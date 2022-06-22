423772c0aefda6e2c06009080b2c5d965576d7c5
pppreap <paulap@Paulas-MacBook-Pro.local>
2022-06-21 23:54:19 -0400

    created questions array and display first question

--- script.js
+++ script.js
@@ -1,41 +1,126 @@
 //declare variables to be used to access html elements
-var timeEl = document.getElementById("timer");
-var scoreEl = document.getElementById("scores-btn");
-var startBtn = document.getElementById("start-btn");
-var mainEl = document.getElementById("container");
-var startpageEl = document.getElementById("startpage");
+const start = document.getElementById("start-btn");
+const timer = document.getElementById("timer");
+const main = document.getElementById("container");
+const startpage = document.getElementById("startpage");
 
 //display time :0 at the start page by accessing element ID
-timeEl.innerHTML = "Time: 0";
+timer.innerHTML = "Time: 0";
 
-//start countdown timer and first question to pop up 
-function startQuiz () {
-startpageEl.setAttribute("style", "display:none");
-startTimer;
 
-//declare variables for timer function start timer at 75 seconds
-var count = 75;
-var startTimer;
+//variables for questions function
+const qtopic = document.getElementById("questions");
+const options = document.querySelector(".options");
+const quiz = document.getElementById("quiz");
 
-countdown = setInterval(function(){
-  console.log(count);
-  count--;
-  timeEl.innerHTML = "Time:" + count;
- 
+const op1 = document.querySelector(".op1");
+const op2 = document.querySelector(".op2");
+const op3 = document.querySelector(".op3");
+const op4 = document.querySelector(".op4");
+
+//array to hold all possible questions and answers as objects 
+let questions = [
+
+  {
+  question: "Commonly used data types DO Not Include:", 
+  op1 : " strings",  
+  op2 : " booleans", 
+  op3 : " alerts", 
+  op4 : " numbers",
+  correct: "3"
+  },
+  
+  {
+  question: "The condition in an if/else statement is enclosed with _____________.",
+  op1 : "quotes", 
+  op2 : "curly brackets", 
+  op3 :"parenthesis", 
+  op4 : "square brackets",
+  correct: "3"
+  },
+  
+  {
+  question: "Arrays in JavaScript can be used to store ___________.",
+  op1 : "numbers and strings",
+  op2 : "Other arrays",
+  op3 : "Boolean",
+  op4 : "All of the above",
+  correct: "4"
+  },
+  
+  {
+  question: "String values must be enclosed within ______ when being assigned to variables.",
+  op1 : "Commas",
+  op2 : "Curly brackets",
+  Op3 : "Quotes",
+  op4 : "Parenthesis",
+  correct: "3"
+  },
+  
+  {
+  question: "A  very useful tool used during development and debugging for printing content to the debugger is:",
+  op1 : "Javascript",
+  op2 : "Terminal/bash",
+  op3 : "For loops",
+  op4 : "console.log",
+  Correct: "4"
+  },
+  ];
+
+const lastIndexQ = questions.length -1;
+let startIndexQ = 0;
+
+//function access questions to change elements 
+function displayQ() {
+  let q = questions[startIndexQ];
+  qtopic.innerHTML = `<h2>${q.question}</h2>`;
+  op1.innerHTML= "1." + q.op1;
+  op2.innerHTML= "2." + q.op2;
+  op3.innerHTML= "3." + q.op3;
+  op4.innerHTML= "4." + q.op4;
+  qtopic.setAttribute("style", "align-self:center; font-size: 40px;");
+  op1.setAttribute("style","color:white;");
+  op2.setAttribute("style","color:white;");
+  op3.setAttribute("style","color:white;");
+  op4.setAttribute("style","color:white;");
+  document.getElementById("timer").value = "Time: " + 75 - count;
+  count--; 
+  
+};
+
+ //declare variables for timer function start timer at 75 seconds
+ var count = 75;
+
+//display timer : time remaining for quiz
+function displayTimer() {
+//timer countdown start using setInterval method
+  countdown= setInterval(function(){
+ //if timer reaches 0 seconds it stops the timer usign clearInterval
   if (count === 0) {
     console.log("Game is over");
     document.getElementById("timer").innerHTML = "Quiz is over";
     clearInterval(countdown); 
-  }
+  } 
+  //display countdown timer on application
+  console.log(count);
+  count;
+  timer.innerHTML = "Time:" + count;
   document.getElementById("timer").value = "Time: " + 75 - count;
-  count--; 
-}, 1000);
-}
 
+  count--; 
+}, 1000); 
+};
+
+//click start button, start countdown timer, and first question to pop up 
+function startQuiz() {
+startpage.setAttribute("style", "display:none");
+displayQ();
+quiz.setAttribute("style", "display:block");
+displayTimer();
+};
 
 //event listener to click of button
-startBtn.addEventListener("click", startQuiz);
-
+start.addEventListener("click", startQuiz);
 
 
 /*
